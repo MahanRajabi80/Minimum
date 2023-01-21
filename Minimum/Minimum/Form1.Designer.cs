c351bfa3536c6036400d9eaf4488e482a27c4b4a
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-01-21 21:25:59 +0330

    Add files via upload

---
+++ Minimum/Minimum/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Minimum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMin = new System.Windows.Forms.Button();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.txtNumber3 = new System.Windows.Forms.TextBox();
+            this.txtNumber4 = new System.Windows.Forms.TextBox();
+            this.txtNumber5 = new System.Windows.Forms.TextBox();
+            this.txtNumber6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnMin
+            // 
+            this.btnMin.Location = new System.Drawing.Point(237, 196);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(75, 23);
+            this.btnMin.TabIndex = 0;
+            this.btnMin.Text = "Min";
+            this.btnMin.UseVisualStyleBackColor = true;
+            this.btnMin.Click += new System.EventHandler(this.btnMin_Click);
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(227, 23);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber1.TabIndex = 1;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(227, 49);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber2.TabIndex = 2;
+            // 
+            // txtNumber3
+            // 
+            this.txtNumber3.Location = new System.Drawing.Point(227, 75);
+            this.txtNumber3.Name = "txtNumber3";
+            this.txtNumber3.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber3.TabIndex = 3;
+            // 
+            // txtNumber4
+            // 
+            this.txtNumber4.Location = new System.Drawing.Point(227, 101);
+            this.txtNumber4.Name = "txtNumber4";
+            this.txtNumber4.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber4.TabIndex = 4;
+            // 
+            // txtNumber5
+            // 
+            this.txtNumber5.Location = new System.Drawing.Point(227, 127);
+            this.txtNumber5.Name = "txtNumber5";
+            this.txtNumber5.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber5.TabIndex = 5;
+            // 
+            // txtNumber6
+            // 
+            this.txtNumber6.Location = new System.Drawing.Point(227, 153);
+            this.txtNumber6.Name = "txtNumber6";
+            this.txtNumber6.Size = new System.Drawing.Size(100, 20);
+            this.txtNumber6.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Enter Number 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Enter Number 2:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(89, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Enter Number 3:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Enter Number 4:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(89, 134);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Enter Number 5:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(89, 160);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Enter Number 6:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumber6);
+            this.Controls.Add(this.txtNumber5);
+            this.Controls.Add(this.txtNumber4);
+            this.Controls.Add(this.txtNumber3);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.btnMin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.TextBox txtNumber3;
+        private System.Windows.Forms.TextBox txtNumber4;
+        private System.Windows.Forms.TextBox txtNumber5;
+        private System.Windows.Forms.TextBox txtNumber6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Minimum/Minimum/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minimum
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMin_Click(object sender, EventArgs e)
+        {
+            int d1, d2, d3, d4, d5, d6, min;
+            d1 = Convert.ToInt32(txtNumber1.Text);
+            d2 = Convert.ToInt32(txtNumber2.Text);
+            d3 = Convert.ToInt32(txtNumber3.Text);
+            d4 = Convert.ToInt32(txtNumber4.Text);
+            d5 = Convert.ToInt32(txtNumber5.Text);
+            d6 = Convert.ToInt32(txtNumber6.Text);
+            min = d1;
+            if (min > d2) min = d2;
+            if (min > d3) min = d3;
+            if (min > d4) min = d4;
+            if (min > d5) min = d5;
+            if (min > d6) min = d6;
+            MessageBox.Show("Min=" + min.ToString(), "Min");
+        }
+    }
+}
